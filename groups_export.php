e7f80529e84771f6a1300a61a029439dca10e927
Steve Stone <steve@digitalstache.com>
2013-07-05 08:41:47 -0400

    - will now export first, last, first and last, email, and id all independently. - No longer *have* to have 'ID' selected. - Download button disappears if no checkboxes are checked or nothing in group ID text box.

--- groups_export.php
+++ groups_export.php
@@ -1,33 +1,23 @@
 <?php 
 /*
 Plugin Name: Groups Export
-Plugin URI: 
-Description: 1-click CSV export for Groups (First Name, Last Name, First Last Name, ID, Email)
+Plugin URI: http://steveostudios.tv/groups-export
+Description: 1-click CSV export for Groups (First Name, Last Name, First and Last Name, ID, Email)
 Author: Steve Stone
 Version: 1.0
-Author URI: 
+Author URI: http://steveostudios.tv
 */
 
-function oscimp_getproducts($product_cnt=1) {
-
-	//Connect to the OSCommerce database
-	$oscommercedb = new wpdb(get_option('ge_group_id'),get_option('ge_export_id'),get_option('ge_export_fname'),get_option('ge_export_lname'),get_option('ge_export_flname'),get_option('ge_export_email'));
-
-	$retval = '';
-	
-	
-	return $retval;
-}
 
 //*************** Admin function ***************
 function groups_export_admin() {
-	include('groups_export_admin.php');
+  include('groups_export_admin.php');
 }
 
-function oscimp_admin_actions() {
-    add_options_page("Groups Export", "Groups Export", 1, "groups-export", "groups_export_admin");
-}
+add_action('admin_menu', 'groups_export_admin_actions');
 
-add_action('admin_menu', 'oscimp_admin_actions');
+function groups_export_admin_actions() {
+	add_options_page('Groups Export', 'Groups Export', 'manage_options', 'groups_export_admin.php', 'groups_export_admin');
+}
 
 ?>
