1a9cf604c02ca9cdd782a20ff77137feb85e3bac
Steve Stone <steve@digitalstache.com>
2013-07-05 08:41:59 -0400

    - will now export first, last, first and last, email, and id all independently. - No longer *have* to have 'ID' selected. - Download button disappears if no checkboxes are checked or nothing in group ID text box.

--- get_csv.php
+++ get_csv.php
@@ -1,54 +1,71 @@
 <?php
-  $ge_group_id = $_POST['ge_group_id'];
-	$ge_export_id = $_POST['ge_export_id'];
-	$ge_export_fname = $_POST['ge_export_fname'];
-	$ge_export_lname = $_POST['ge_export_lname'];
-	$ge_export_flname = $_POST['ge_export_flname'];
-	$ge_export_email = $_POST['ge_export_email'];
-	
-  include_once($_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
-  
   header("Content-type: text/csv");  
   header("Cache-Control: no-store, no-cache");  
-  header('Content-Disposition: attachment; filename="group-email.csv"');  
-    
-  $outstream = fopen("php://output",'w');  
+  header('Content-Disposition: attachment; filename="groups-export.csv"');
+  include_once( "../../../wp-load.php" );
 
-  global $wpdb;
-  $user_ids = $wpdb->get_results( 
-  	"
-  	SELECT * 
-  	FROM `wp_groups_user_group` 
-  	WHERE `group_id` = $ge_group_id
-  	"
-  );
-  $result = array();
-  foreach ( $user_ids as $user_id ) {
-    $user = $wpdb->get_results(
-      "
-      SELECT * 
-      FROM  `wp_users` 
-      WHERE  `ID` =  $user_id->user_id
-      "
+  function ge_csv_export() {
+    $outstream = fopen("php://output",'w');
+    $ge_group_id = (isset($_POST['ge_group_id']))? $_POST['ge_group_id'] : null;
+		$ge_export_id = (isset($_POST['ge_export_id']))? $_POST['ge_export_id'] : null;
+		$ge_export_fname = (isset($_POST['ge_export_fname']))? $_POST['ge_export_fname'] : null;
+		$ge_export_lname = (isset($_POST['ge_export_lname']))? $_POST['ge_export_lname'] : null;
+		$ge_export_flname = (isset($_POST['ge_export_flname']))? $_POST['ge_export_flname'] : null;
+		$ge_export_email = (isset($_POST['ge_export_email']))? $_POST['ge_export_email'] : null;
+  
+    global $wpdb;
+    
+    $user_ids = $wpdb->get_results( 
+    	"
+    	SELECT * 
+    	FROM `wp_groups_user_group` 
+    	WHERE `group_id` = $ge_group_id
+    	"
     );
+    $result = array();
+    foreach ( $user_ids as $user_id ) {
+      $result[$user_id->user_id] = array();
+      // ID
+      if(isset($ge_export_id) && $ge_export_id == 'on') {
+        $result[$user_id->user_id]['id'] = $user_id->user_id;
+      }
+      // First and/or Last Name
+      if((isset($ge_export_fname) && $ge_export_fname == 'on')) {
+        $user_name = $wpdb->get_row(
+          "
+          SELECT 
+            (SELECT `meta_value` FROM `wp_usermeta` WHERE `user_id` = $user_id->user_id AND `meta_key` = 'first_name') AS first_name,
+            (SELECT `meta_value` FROM `wp_usermeta` WHERE `user_id` = $user_id->user_id AND `meta_key` = 'last_name') AS last_name
+          "
+        );
+        if((isset($ge_export_fname) && $ge_export_fname == 'on')) {
+          $result[$user_id->user_id]['fname'] = $user_name->first_name;
+        }
+        if((isset($ge_export_lname) && $ge_export_lname == 'on')) {
+          $result[$user_id->user_id]['lname'] = $user_name->last_name;
+        }
+        if((isset($ge_export_flname) && $ge_export_flname == 'on')) {
+          $result[$user_id->user_id]['flname'] = $user_name->first_name . ' ' . $user_name->last_name;
+        }
+      }
+      // Email
+      if(isset($ge_export_email) && $ge_export_email == 'on') {    
+        $user_email = $wpdb->get_var(
+          "
+          SELECT `user_email` 
+          FROM  `wp_users` 
+          WHERE  `ID` =  $user_id->user_id
+          "
+        );  
+        $result[$user_id->user_id]['email'] = $user_email;
+      }
+    }
 
-    array_push($result, $user);
+    foreach ($result as $row) {
+      fputcsv($outstream, $row, ',', '"');
+    }
+    fclose($outstream);
   }
-
-
-  foreach ($result as $row) {
-    $row_inner = array();
-    if ($ge_export_id == 'on') {
-      array_push($row_inner, $row[0]->ID);
-    }
-    if ($ge_export_email == 'on') {
-      array_push($row_inner, $row[0]->user_email);
-    }
-    
-    //$row_inner = array($row[0]->ID,$row[0]->user_email);
-
-    fputcsv($outstream, $row_inner, ',', '"');
-  } 
-    
-  fclose($outstream);
+  ob_end_clean();
+  ge_csv_export();
 ?>
